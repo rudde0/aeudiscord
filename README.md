5bf213b4435ef90ef801acaeb9b0dae2d89475c2
Hazar <hazar.akbuz@outlook.com>
2020-10-02 18:16:01 +0300

    First release

---
+++ README.md
@@ -0,0 +1,1 @@
+SS, eğlence amaçlı yazılmış discord botu.
---
+++ bot.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const activities = require('./assets/activities');
+
+client.on('ready', () => {
+	console.log(`Bot, ${client.user.tag} adiyla baslatildi. Kimlik: ${client.user.id}`);
+	client.setInterval(() => {
+		const activity = activities[Math.floor(Math.random() * activities.length)];
+		client.user.setStatus('available')
+		client.user.setPresence({
+			game: {
+				name: activity.text,
+				type: activity.type
+			}
+		});
+    }, 60000);
+});
+client.on('error', console.error);
+
+client.on('guildMemberAdd', member => {
+	member.addRole(`761605971067797534`);
+});
+
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+
+const events = {
+	MESSAGE_REACTION_ADD: 'messageReactionAdd',
+	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
+};
+
+client.login(process.env.bot_tokeni);
